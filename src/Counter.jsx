664fda77a280c8c172a1d88597cc1ed8f85db9f3
Abhishek Anand <anandabhishek3607@gmail.com>
2023-11-06 00:34:14 +0530

    working with useEffects in React

---
+++ src/Counter.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  useEffect(
+    function myEffect() {
+      console.log("My effect was called!!");
+    },
+    [count, name]
+  );
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={increment}>+1</button>
+      <p>Name:{name}</p>
+      <input value={name} onChange={handleChange} type="text" />
+    </div>
+  );
+}
